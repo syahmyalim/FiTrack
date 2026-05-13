f13eef5438a74cce830e7f6cbf30edeaa11b4143
syahmyalim <syahmyalim@gmail.com>
2026-05-13 17:23:08 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/* ═══════════════════════════════════════
+   GLOBAL CONFIGURATION
+═══════════════════════════════════════ */
+
+const CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbykoClCGJJFOKKrUORkLzdaFwLsikZvXLftqQg3yDg_09psCyqCBALY1weZQqLcs7x8KA/exec',
+  ALLOWED_EMAILS: ['syahmyalim@gmail.com', 'nabila9782@gmail.com']
+};
