468602ca77ff8af90fda290be913d10867b295f4
syahmyalim <syahmyalim@gmail.com>
2026-05-13 17:36:33 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,9 @@
 ═══════════════════════════════════════ */
 
 const CONFIG = {
+  // Your active Google Apps Script URL
   GAS_URL: 'https://script.google.com/macros/s/AKfycbykoClCGJJFOKKrUORkLzdaFwLsikZvXLftqQg3yDg_09psCyqCBALY1weZQqLcs7x8KA/exec',
+  
+  // Security lockdown matching your Apps Script
   ALLOWED_EMAILS: ['syahmyalim@gmail.com', 'nabila9782@gmail.com']
 };
